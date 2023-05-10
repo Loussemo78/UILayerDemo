178160ab6def89e01905c3b15e81c1dfd1b21d8e
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-10 16:48:27 +0200

    fun MainScreen from MainActivity in progress

--- app/src/main/java/com/example/uilayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/uilayerdemo/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.uilayerdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uilayerdemo.ui.theme.UILayerDemoTheme
 
@@ -34,6 +36,33 @@ class MainActivity : ComponentActivity() {
 fun MainScreen(
     modifier: Modifier = Modifier,
     counterViewModel: CounterViewModel = viewModel()
-){
+) {
+    val screenState = counterViewModel.screenState.value
+    val scaffoldState = rememberScaffoldState()
 
+    Scaffold(scaffoldState = scaffoldState) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(50.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(
+                modifier = modifier.fillMaxWidth(),
+                text = screenState.displayingResult,
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color.DarkGray
+            )
+            
+            OutlinedTextField(
+                value = screenState.inputValue,
+                onValueChange = {
+
+                }
+            )
+
+        }
+
+    }
 }
